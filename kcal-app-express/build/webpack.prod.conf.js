0e6edabecfe09b8e9b559b9895c201b2c8d9f297
Magyari Zoltán <devbysp@proton.me>
2023-08-31 17:29:29 +0300

    Webpack works!

---
+++ kcal-app-express/build/webpack.prod.conf.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const base = require('./webpack.base.conf');
+
+base.mode = 'production';
+base.plugins = [
+  new webpack.ContextReplacementPlugin(
+    /express\/lib/,
+    resolve(__dirname, '../node_modules'),
+    {
+      cors: 'cors',
+      sqlite3: 'sqlite3',
+      winston: 'winston',
+    },
+  ),
+];
+base.stats = {
+  warningsFilter: /require\.extensions/,
+};
+
+module.exports = base;
